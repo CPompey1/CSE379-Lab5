d6c98bfaec8d12eb34a33796ee77725430ecb2ae
Cristian Pompey <cpompey10@gmail.com>
2023-03-28 00:51:53 -0400

    Wrote everything except uart handler

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,49 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+S_SRCS += \
+../lab5_assembly.s 
+
+C_SRCS += \
+../lab5wrapper.c \
+../tm4c123gh6pm_startup_ccs.c 
+
+S_DEPS += \
+./lab5_assembly.d 
+
+C_DEPS += \
+./lab5wrapper.d \
+./tm4c123gh6pm_startup_ccs.d 
+
+OBJS += \
+./lab5_assembly.obj \
+./lab5wrapper.obj \
+./tm4c123gh6pm_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"lab5_assembly.obj" \
+"lab5wrapper.obj" \
+"tm4c123gh6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"lab5wrapper.d" \
+"tm4c123gh6pm_startup_ccs.d" 
+
+S_DEPS__QUOTED += \
+"lab5_assembly.d" 
+
+S_SRCS__QUOTED += \
+"../lab5_assembly.s" 
+
+C_SRCS__QUOTED += \
+"../lab5wrapper.c" \
+"../tm4c123gh6pm_startup_ccs.c" 
+
+
--- lab5_assembly.s
+++ lab5_assembly.s
@@ -2,6 +2,11 @@
 
 	.global prompt
 	.global mydata
+	.global switch_presses_str
+	.global key_presses_str
+	.global num_1_string
+	.global num_2_string
+
 
 prompt:	.string "Your prompt with instructions is place here", 0
 mydata:	.byte	0x20	; This is where you can store data.
@@ -9,6 +14,11 @@ mydata:	.byte	0x20	; This is where you can store data.
 			; (initialized to 0x20) at the label mydata.
 			; Halfwords & Words can be stored using the
 			; directives .half & .word
+switch_presses_str: 	.string "Switch Presses: ", 0
+key_presses_str:  		.string "Key Presses: ", 0
+num_1_string: 	.string "Place holder too long", 0
+num_2_string:  	.string "Place holder too long", 0
+
 
 	.text
 
@@ -26,7 +36,15 @@ mydata:	.byte	0x20	; This is where you can store data.
 
 ptr_to_prompt:		.word prompt
 ptr_to_mydata:		.word mydata
+ptr_to_switch_presses_str:	.word switch_presses_str
+ptr_to_key_presses_str:	.word key_presses_str
+ptr_to_num_1_string:	.word num_1_string
+ptr_to_num_2_string:	.word num_2_string
 
+;***************Data packet orginization*******************************
+;	|SwitchPresses	|KeyPresses	|End Flag	|Nothing|
+;	0				8			16			24		32
+;**********************************************************************
 lab5:	; This is your main routine which is called from your C wrapper
 	PUSH {lr}   		; Store lr to stack
 	ldr r4, ptr_to_prompt
@@ -39,6 +57,15 @@ lab5:	; This is your main routine which is called from your C wrapper
 	; This is where you should implement a loop, waiting for the user to
 	; enter a q, indicating they want to end the program.
 
+	MOV r0,r5
+	bl output_string
+
+main_print_data_loop:
+	bl print_data
+	LDRB r0,[r4,#16]
+	cmp r0, #1
+	bne main_print_data_loop
+
 	POP {lr}		; Restore lr from the stack
 	MOV pc, lr
 
@@ -47,6 +74,10 @@ lab5:	; This is your main routine which is called from your C wrapper
 uart_interrupt_init:
 
 	; Your code to initialize the UART0 interrupt goes here
+	;enable UART0 6th bit
+
+	;MUST SET END BIT IF Q IS TYPED
+
 
 	MOV pc, lr
 
@@ -56,6 +87,41 @@ gpio_interrupt_init:
 	; Your code to initialize the SW1 interrupt goes here
 	; Don't forget to follow the procedure you followed in Lab #4
 	; to initialize SW1.
+
+	;enable interrupt sense register GPIOS
+	MOV r0, #0x5404
+	MOVT r0, #0x4002
+	LDR r1, [r0]
+	ORR r1,r1, #16
+	STR r1,[r0]
+
+	;Enable interupt direction(s) ;Consider removing this when debugging
+	MOV r0, #0x5408
+	MOVt r0, #0x4002
+	LDR r1, [r0]
+	ORR r1,r1,#16
+	STR r1,[r0]
+
+	;Enable rising edge interrupt
+	MOV r0, #0x540C
+	MOVt r0, #0x4002
+	LDR r1, [r0]
+	ORR r1,r1,#16
+	STR r1,[r0]
+
+	;Enable nterrupt GPIOIM
+	MOV r0, #0x5410
+	MOVt r0, #0x4002
+	LDR r1, [r0]
+	ORR r1,r1,#16
+	STR r1,[r0]
+
+	;Convfigure Procesor to Allow GPIO Port F to interrupt processor
+	MOV r0, #0xE100
+	MOVt r0, #0xE000
+	LDR r1, [r0]
+	ORR r1,r1, #1073741824 ;lol 2^30
+	STR r1,[r0]
 
 	MOV pc, lr
 
@@ -74,6 +140,25 @@ Switch_Handler:
 	; Your code for your UART handler goes here.
 	; Remember to preserver registers r4-r11 by pushing then popping
 	; them to & from the stack at the beginning & end of the handler
+	PUSH {r4-r11}
+
+	;clear interrupt register GPIOICR
+	MOV r0, #0x541C
+	MOVT r0, #0x4002
+	LDR r1,[r0]
+	ORR r1, r1,#16
+	STR r1, [r0]
+
+	;Incrament switch presses
+	ldr r0,ptr_to_mydata
+	LDRB r1,[r0];Modify first byte
+	ADD r1, r1,#1
+	STRB r1,[r0]
+
+
+
+
+	POP {r4-r11}
 
 	BX lr       	; Return
 
@@ -90,7 +175,133 @@ Timer_Handler:
 
 	BX lr       	; Return
 
+;************************************************************************START PRINT DATA************************************************************************
+;print_data
+;	-prints data loaded in data word block as well as a bar graph
+;	- uses no unpreserved registers
+print_data:
+	;load switch presses
+	ldr r0, ptr_to_mydata
+	LDRB r1, [r0]
 
+	;Print switch presses
+	PUSH {r0,r1}
+	ldr r0, ptr_to_switch_presses_str
+	bl output_string
+	POP {r0,r1}
+	PUSH {r0,r1}
+	MOV r0,r1
+	ldr r1, ptr_to_num_1_string
+	bl int2string
+	MOV r0,r1
+	bl output_string
+	POP {r0,r1}
+
+	;Print newline
+
+	PUSH {r0,r1,r2}
+	MOV r0, #0x0A
+	bl output_character
+	PUSH {r0,r1,r2}
+
+	;print carriage return
+	PUSH {r0,r1,r2}
+	MOV r0, #0x0d
+	bl output_character
+	PUSH {r0,r1,r2}
+
+	;load key presses
+	ldr r0, ptr_to_mydata
+	LDRB r1, [r0,#8]
+
+	;Print key presses
+	PUSH {r0,r1}
+	ldr r0, ptr_to_key_presses_str
+	bl output_string
+	POP {r0,r1}
+	PUSH {r0,r1}
+	MOV r0,r1
+	ldr r1,ptr_to_num_2_string
+	bl int2string
+	MOV r0,r1
+	bl output_string
+	POP {r0,r1}
+
+	;Print newline
+
+	PUSH {r0,r1,r2}
+	MOV r0, #0x0A
+	bl output_character
+	PUSH {r0,r1,r2}
+
+	;print carriage return
+	PUSH {r0,r1,r2}
+	MOV r0, #0x0d
+	bl output_character
+	PUSH {r0,r1,r2}
+
+
+
+
+	;Print Bargraph
+	;load switch presses
+	ldr r0, ptr_to_mydata
+	LDRB r1, [r0]
+	;For all switchpresses print x's
+print_switch_presses_loop:
+	PUSH {r0,r1,r2}
+	MOV r0, #120
+	bl output_character
+	POP {r0,r1,r2}
+	ADD r1, r1,#-1
+	CMP r1, #0
+	BNE print_switch_presses_loop
+
+	;Print newline
+
+	PUSH {r0,r1,r2}
+	MOV r0, #0x0A
+	bl output_character
+	PUSH {r0,r1,r2}
+
+	;print carriage return
+	PUSH {r0,r1,r2}
+	MOV r0, #0x0d
+	bl output_character
+	PUSH {r0,r1,r2}
+
+	;Load key presses
+	ldr r0, ptr_to_mydata
+	LDRB r1, [r0,#8]
+
+	;For all keypresses print x's
+print_key_presses_loop:
+	PUSH {r0,r1,r2}
+	MOV r0, #120
+	bl output_character
+	POP {r0,r1,r2}
+	ADD r1, r1, #-1
+	CMP r1, #0
+	BNE print_key_presses_loop
+	;Print newline
+
+	PUSH {r0,r1,r2}
+	MOV r0, #0x0A
+	bl output_character
+	PUSH {r0,r1,r2}
+
+	;print carriage return
+	PUSH {r0,r1,r2}
+	MOV r0, #0x0d
+	bl output_character
+	PUSH {r0,r1,r2}
+
+
+
+
+
+	MOV PC,LR
+;************************************************************************END PRINT DATA************************************************************************
 simple_read_character:
 
 	MOV PC,LR      	; Return
@@ -109,6 +320,153 @@ read_string:
 output_string:
 
 	MOV PC,LR      	; Return
+
+uart_init:
+	MOV PC, LR
+
+;****************************************************************HELPER SUBROUTINES************************************************************************
+int2string:
+	PUSH {lr}   ; Store register lr on stack
+
+	;Push r0 and r1 before the call to integer_digits
+	PUSH {r0,r1}
+
+	;r1 = integer_digits(r0,r1-doesntmatter)
+	MOV r1, #0x0
+	BL integer_digit
+
+	;Store (numDigits - 1) in r2
+	SUB r2, r0, #0x1
+
+	;Pop old r0 & r1 from stack
+	POP {r0,r1}
+
+;Integer_digit
+;Inputs: r0	- Decimal value
+;		 r1 - n place in decimal value to find
+;
+;Outputs: r0 - nth place digit in decimal value
+;		  r1 - number of digits to place
+integer_digit:			; Your code for the integer_digit routine goes here.
+	PUSH{lr}
+	CMP r0, #0
+	BEQ ALMOSTFINISH
+	cmp r0,#10
+	BLT ALMOSTFINISH1
+	MOV r2, #10			;10
+	MOV r3, #1			 ;Decimal digit counter
+	MOV r4, r0			;inital r0 with deciaml place to be shifted
+
+
+countDigLoop:			;Loop until r4 is shifted to the left most digit
+
+	ADD r3, r3, #1		;Incrament r3
+	SDIV r4, r4, r2		;Shift r4 by one decimal place
+
+
+	CMP r4, #10			;r4 will be at its left most digit when r4 < 10
+	BGE countDigLoop
+
+						;Num of decimal digits stored in r3
+	PUSH {r0,r1}
+	bl nthPlace
+
+	MOV r4, r0
+	POP {r0,r1}
+	MOV r0, r3
+	MOV r1, r4
+
+	b FINISH
+ALMOSTFINISH:
+	mov r0, #0
+	mov r1, #0
+
+ALMOSTFINISH1:
+	MOV r1,r0
+	MOV r0,#1
+
+FINISH:
+	POP {lr}
+	MOV pc, lr
+
+
+nthPlace:				;find the digit in the r1 = nth place of r0(r2 used
+	PUSH {lr}
+	PUSH {r2,r3,r4}
+	MOV r2,r0
+	MOV r3,r1
+
+
+	MOV r0, #10
+	bl POW			;r0 = 10^r1(n)
+
+
+	SDIV r0, r2, r0	;r0 = val/10^n & r1 = n
+	MOV r1, #10
+	PUSH {r0,r1}			;Store r0 & r1
+	bl MOD				; r0 = r0 mod 10
+	MOV r2, r0			;Move result from r0 to r2
+	POP {r0,r1}			; Get r0 and r1 back
+
+
+	;r0 = divided val, r1 = 10 , r2 = dvidedVal mod 10
+
+	CMP r2, #0
+	BEQ endNthPlaceLoop
+	MOV r3, #0
+nthPlaceLoop:
+
+	ADD r3, r3, #1
+	SUB r0, r0, #1
+	push {r0,r1}
+	MOV r1, #10
+	push {LR}
+	BL MOD
+	pop {lr}
+	MOV r2, r0
+	POP {r0,r1}
+	CMP r2, #0
+	BNE nthPlaceLoop
+
+endNthPlaceLoop:
+	MOV r0,r3
+	pop {r2,r3,r4}
+	pop {lr}
+	MOV pc, lr
+
+MOD:					;Take r0 = r0 mod r1		(r0 & r1 as arguments. r2 is used)
+	PUSH {lr}
+	PUSH {r2}
+	SDIV r2, r0, r1		;r2 = floor(r0/r1)
+	MUL r1, r1, r2		;r1 = r1 * r2
+	SUB r0,	r0, r1,		;r0 = r0 mod r1
+	POP {r2}
+	POP {lr}
+	MOV pc, lr
+
+POW:					;r0 has base, r1 has exponential
+	PUSH {lr}
+	PUSH {r2}
+	MOV r2, #1
+	CMP r1, #0		;Stop if the exponential is 0
+	BEQ donePow1
+
+powLoop:
+					;Push the registers that were used in the subroutine
+	MUL r2, r2, r0
+	SUB r1, r1, #1
+	CMP r1, #0
+	BNE powLoop			;Stop if the exponential is 0
+
+
+
+donePow1: 				;Pow exit
+	MOV r0, r2			;Return stored in r0
+	POP {r2}
+	POP {lr}
+	MOV pc,lr
+;****************************************************************END HELPER SUBROUTINES************************************************************************
+
 
 
 	.end
